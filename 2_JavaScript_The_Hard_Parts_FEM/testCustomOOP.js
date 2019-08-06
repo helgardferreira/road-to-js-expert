502133d1dd2d059ce76d810768f13f2f180e408a
Helgard Ferreira <hrfer3@gmail.com>
2019-08-06 14:52:01 +0200

    Add ES6 Class For Control Group

--- 2_JavaScript_The_Hard_Parts_FEM/testCustomOOP.js
+++ 2_JavaScript_The_Hard_Parts_FEM/testCustomOOP.js
@@ -13,26 +13,26 @@ PersonConstructor.prototype.introduce = function introduce() {
   console.log(`Hi, my name is ${this.name}`);
 };
 
-// class PersonClass {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//     this.greet = () => {
-//       console.log('hello');
-//     };
-//   }
+class RealPersonClass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.greet = () => {
+      console.log('hello');
+    };
+  }
 
-//   introduce() {
-//     console.log(`Hi, my name is ${this.name}`);
-//   }
-// }
-// const realPerson = new PersonClass('Jim', 65);
+  introduce() {
+    console.log(`Hi, my name is ${this.name}`);
+  }
+}
+const realPerson = new RealPersonClass('John', 43);
 
 const simPersonConstructor = myNew(PersonConstructor, 'Vicky', 25);
 const simPersonClass = PersonClass('Jim', 65);
 
 // test myNew with constructor
-console.log('---------TestNew---------');
+console.log('---------Constructor---------');
 console.log(simPersonConstructor);
 console.log(simPersonConstructor.name);
 console.log(simPersonConstructor.age);
@@ -40,9 +40,17 @@ console.log(simPersonConstructor.__proto__);
 simPersonConstructor.introduce();
 
 // test custom class
-console.log('---------Control---------');
+console.log('------------Class------------');
 console.log(simPersonClass);
 console.log(simPersonClass.name);
 console.log(simPersonClass.age);
 console.log(simPersonClass.__proto__);
 simPersonClass.introduce();
+
+// control
+console.log('-----------Control-----------');
+console.log(realPerson);
+console.log(realPerson.name);
+console.log(realPerson.age);
+console.log(realPerson.__proto__);
+realPerson.introduce();
